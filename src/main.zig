6a2f234400c108558ded151e729a273b59a8818d
ElrohirGT <elrohirgt@gmail.com>
2025-01-14 13:03:40 -0600

    feat: Added file testing in zig

--- src/main.zig
+++ src/main.zig
@@ -5,6 +5,13 @@ const TokenType = enum {
     IntLiteral,
     Assignment,
     TopLevelName,
+    Variable,
+
+    // Operation tokens
+    SumOperator,
+    MinusOperator,
+    MultOperator,
+    DivisionOperator,
 };
 
 const Token = struct {
@@ -37,10 +44,10 @@ pub fn main() !void {
     var file = std.fs.cwd().openFile(filePath, .{}) catch unreachable;
     defer file.close();
 
-    _ = try parse(alloc, file.reader().any());
+    _ = try tokenize(alloc, file.reader().any());
 }
 
-fn parse(alloc: std.mem.Allocator, reader: std.io.AnyReader) !std.ArrayList(Token) {
+fn tokenize(alloc: std.mem.Allocator, reader: std.io.AnyReader) !std.ArrayList(Token) {
     var char_buffer: [1]u8 = undefined;
     var parser_state = State.None;
     var tokens = std.ArrayList(Token).init(alloc);
@@ -93,21 +100,28 @@ fn parse(alloc: std.mem.Allocator, reader: std.io.AnyReader) !std.ArrayList(Toke
                 switch (char) {
                     ' ', '\t', '\r', '\n' => continue,
                     '0'...'9' => {
-                        var buffer = std.ArrayList(u8).init(alloc);
-                        defer buffer.deinit();
-                        try buffer.append(char);
+                        const value: []const u8 = get_value: {
+                            var buffer = std.ArrayList(u8).init(alloc);
+                            defer buffer.deinit();
+                            try buffer.append(char);
 
-                        reader.streamUntilDelimiter(buffer.writer(), ' ', null) catch |err| switch (err) {
-                            error.EndOfStream => {},
-                            else => {
-                                return err;
-                            },
+                            while (true) {
+                                const byte = reader.readByte() catch |err| switch (err) {
+                                    error.EndOfStream => break,
+                                    else => return err,
+                                };
+                                switch (byte) {
+                                    ' ', '\t', '\r', '\n' => break,
+                                    else => try buffer.append(byte),
+                                }
+                            }
+
+                            break :get_value try buffer.toOwnedSlice();
                         };
-                        const argName = try buffer.toOwnedSlice();
 
-                        try tokens.append(.{ .value = argName, .type = .IntLiteral });
+                        try tokens.append(.{ .value = value, .type = .IntLiteral });
                     },
-                    else => unreachable,
+                    else => {},
                 }
             },
         }
@@ -133,7 +147,7 @@ test "Can tokenize simple body" {
     try expectedTokens.append(.{ .type = .Assignment });
     try expectedTokens.append(.{ .type = .IntLiteral, .value = "5" });
 
-    var tokens = try parse(alloc, stream.reader().any());
+    var tokens = try tokenize(alloc, stream.reader().any());
     defer tokens.deinit();
     defer for (tokens.items) |value| {
         value.deinit(alloc);
@@ -148,6 +162,103 @@ test "Can tokenize simple body" {
             try std.testing.expectEqual(expected.value, token.value);
         }
     }
+}
+
+test "Test files" {
+    // var originDir = std.fs.cwd();
+    // try std.testing.expectEqualStrings("", try originDir.realpathAlloc(std.testing.allocator, "."));
+    var dir = try std.fs.cwd().openDir("src/testFiles", .{ .iterate = true });
+    defer dir.close();
+
+    var walker = try dir.walk(std.testing.allocator);
+    defer walker.deinit();
+
+    var no_failures = true;
+    while (try walker.next()) |entry| {
+        if (entry.kind != .file) {
+            continue;
+        }
+
+        const log = std.log.scoped(.file_scope);
+
+        // try std.testing.expectEqualStrings("", entry.path);
+
+        const file = try dir.openFile(entry.path, .{ .mode = .read_only });
+        // const file = try std.fs.openFileAbsolute(entry.path, .{ .mode = .read_only });
+        defer file.close();
+
+        var contentBuffer = std.ArrayList(u8).init(std.testing.allocator);
+        defer contentBuffer.deinit();
+
+        try file.reader().readAllArrayList(&contentBuffer, 10_000);
+        const fileContents = try contentBuffer.toOwnedSlice();
+        defer std.testing.allocator.free(fileContents);
+
+        var iter = std.mem.splitSequence(u8, fileContents, "---");
+        const contents = iter.next() orelse {
+            log.err("File `{s}` no language content supplied! Please write some vulpes code and then write `---` to separate the expected content!", .{entry.path});
+            return error.NoLanguageContentSupplied;
+        };
+        var stream = std.io.fixedBufferStream(contents);
+
+        const expectedContents = iter.next() orelse {
+            log.err("File `{s}` no expected tokens supplied! Please write some vulpes code and then write `---` to separate the expected content!", .{entry.path});
+            return error.NoExpectedTestTokensSupplied;
+        };
+        var expectedTokens = std.ArrayList(Token).init(std.testing.allocator);
+        defer expectedTokens.deinit();
+        // defer for (expectedTokens.items) |value| {
+        //     value.deinit(std.testing.allocator);
+        // };
+        { // Generating expected contents...
+
+            var expectedIter = std.mem.splitScalar(u8, expectedContents, '\n');
+            var i: i32 = 0;
+            while (expectedIter.next()) |line| : (i += 1) {
+                if (line.len == 0) {
+                    continue;
+                }
+
+                var lineIter = std.mem.splitScalar(u8, line, ' ');
+                const type_str = lineIter.next() orelse {
+                    log.err("File `{s}`, {} lines after `---`. No token supplied!", .{ entry.path, i + 1 });
+                    return error.NoTokenTypeSupplied;
+                };
+                const value = lineIter.next();
+                const token_type = std.meta.stringToEnum(TokenType, type_str) orelse {
+                    log.err("File `{s}`, {} lines after `---`. Unknown token supplied `{s}`!", .{ entry.path, i + 1, type_str });
+                    return error.InvalidTokenType;
+                };
+
+                try expectedTokens.append(.{ .value = value, .type = token_type });
+            }
+        }
+
+        var tokens = try tokenize(std.testing.allocator, stream.reader().any());
+        defer tokens.deinit();
+        defer for (tokens.items) |value| {
+            value.deinit(std.testing.allocator);
+        };
+
+        const file_test_result = file_test_result: {
+            for (tokens.items, 0..) |token, i| {
+                const expected = expectedTokens.items[i];
+                std.testing.expectEqual(expected.type, token.type) catch |err| break :file_test_result err;
+                if (expected.value != null and token.value != null) {
+                    std.testing.expectEqualStrings(expected.value.?, token.value.?) catch |err| break :file_test_result err;
+                } else {
+                    std.testing.expectEqual(expected.value, token.value) catch |err| break :file_test_result err;
+                }
+            }
+        };
+
+        file_test_result catch |err| {
+            log.err("Failed test for file: `{s}`. \n ERROR: {}", .{ entry.path, err });
+            no_failures = false;
+        };
+    }
+
+    try std.testing.expect(no_failures);
 }
 
 test "simple test" {
